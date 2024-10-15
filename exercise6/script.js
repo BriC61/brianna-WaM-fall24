7fdc276c40b23b274dbe49b35fcd29877f1c9595
BriC <53392021+BriC61@users.noreply.github.com>
2024-10-15 13:56:59 -0400

    Delete exercise6 directory

--- exercise6/script.js
+++
@@ -1,9 +0,0 @@
-console.log('dark mode');
-
-$(document).ready(function(){
-    $('.toggle').click(function(){
-        $('.toggle').toggleClass('dark-toggle');
-        $('p').toggleClass('dark-text');
-        $('body').toggleClass('dark-bg');
-    });
-});
