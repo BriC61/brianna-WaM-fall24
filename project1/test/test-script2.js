a395553a7ae093c8ce51a262e1f05f24df1a980d
BriC <53392021+BriC61@users.noreply.github.com>
2024-10-22 18:07:54 -0400

    update
    - fixed dragging so that it's smooth (disable img drag)
    - filter: drop-shadow works now?
    - transition should be applied to original class, not on the pseudo class

--- project1/test/test-script2.js
+++ project1/test/test-script2.js
@@ -1,41 +1,17 @@
-const menuButton = document.getElementById('menu-button');
-const menuModal = document.getElementById('menu-modal');
-const closeMenuModal = document.getElementById('close-menu-modal');
-const clearTable = document.getElementById('clear-table');
-
-console.log("loaded constants");
-
-menuButton.addEventListener('click', function() {
-    $('.menu-modal').addClass('visible');
-    $('.menu-modal').removeClass('hidden');
-
-});
-closeMenuModal.addEventListener('click', function() {
-    $('.menu-modal').removeClass('visible'); 
-    $('.menu-modal').addClass('hidden');
-});
-
-window.addEventListener('click', (e) => {
-    if (e.target === menuModal) {
-        $('.menu-modal').removeClass('visible'); 
-        $('.menu-modal').addClass('hidden');
-    }
-});
-
-
-clearTable.addEventListener('click', () => {
-    const allImages = document.querySelectorAll('body > img');
-    allImages.forEach(img => img.remove());
-});
-
-console.log('working code ready');
 /////
 //WORKING draggable elements
+
+//disable img drag
+console.log("loaded constants");
+var food_img = document.getElementsByClassName('food-img visible');
+for(var i = 0; i< food_img.length; i++){
+    food_img[i].ondragstart = function(){return false;}
+}
+
+//dragging code
 let newX=0, newY=0, startX=0, startY=0;
 
-//why doesn't querySelector work?
 const foodContainer = document.getElementById('food-container');
-// const foodContainer = document.querySelectorAll('.food-container');
 
 foodContainer.addEventListener('mousedown', mouseDown)
 
@@ -69,21 +45,17 @@ function mouseUp(e){
 
 //////
 
+
+
 foodContainer.addEventListener('click', toggle)
 function toggle(e){
     $('.food-img').toggleClass('hidden');
     $('.memory').toggleClass('visible');
+    console.log(document.getElementsByClassName(".memory"));
 }
 
 
 //////
 //pre-place a single element (generate multpile instances of div container?), display = visible, 
 //needs to be draggable, currently on hover it displays UNSTYLED text
-const menuContent = document.getElementById('menu-content');
 
-menuContent.addEventListener('click', (e) =>{
-    if (e.target.classList.contains('food-item' && 'xi-fan')) {
-        $('.xi-fan').css('display','visible');
-
-    }
-});
